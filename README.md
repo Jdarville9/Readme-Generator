49ca6bea44840722ac8aa53585d786579ccc623a
Joseph Darville <joseph.darville9@gmail.com>
2022-06-19 00:03:54 -0400

    created gitignore file

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Readme-Generator
-# Description
-# Table of Contents
+
+
+![License](https://img.shields.io/static/v1?label=license&message=Apache-2.0&color=blue)
+
+# j
+## Description
+j
+## Table of Contents
 1. [Installation](#installation)
 2. [Usage](#usage)
 3. [License](#license)
@@ -8,9 +13,31 @@
 5. [Tests](#tests)
 6. [Questions](#questions)
 
-# Installation
-# Usage
-# License
-# Contributing
-# Tests
-# Questions
+## Installation
+j
+
+## Usage
+j
+
+## Contributing
+j
+
+
+## License
+This application is covered under Apache-2.0 licensing
+
+
+License link:
+https://choosealicense.com/licenses/Apache-2.0/
+
+
+## Tests
+
+
+## Questions
+
+If you have any questions regaurding this project, please feel free to contact me via:
+
+* Github: https://github.com/j
+* Email: j
+  
--- index.js
+++ index.js
@@ -1,15 +1,118 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs'); 
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Prompt questions
+const promptQuestions = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+              if (titleInput) {
+                return true;
+              } else {
+                console.log('Please enter a title for your project!');
+                return false;
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description of your project',
+            validate: descriptionInput => {
+              if (descriptionInput) {
+                return true;
+              } else {
+                console.log('Please enter a description of your project!');
+                return false;
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter instructions on how to install this application'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter a description of how to use this application'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter information regaurding all contributions to this project'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Provide information on how to test this application'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose which license was used, if any',
+            choices: ['Apache-2.0', 'BSD-2-Clause', 'BSD-3-Clause', 'AGPL-3.0', 'MIT', 'MPL-2.0', 'EPL-2.0', 'None Apply']
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub Username?',
+            validate: githubInput => {
+              if (githubInput) {
+                return true;
+              } else {
+                console.log('Please provide your GitHub username!');
+                return false;
+              }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your E-mail address?',
+            validate: titleInput => {
+              if (titleInput) {
+                return true;
+              } else {
+                console.log('Please enter your E-mail address!');
+                return false;
+              }
+            }
+        },
+    ])
+    // Return the answers
+    .then(data => {
+        return data
+    });    
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data , err => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("README.md has been created")
+            }
+        });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptQuestions()
+    .then(data => {
+        return generateMarkdown(data);
+    })
+    .then(data => {
+        return writeToFile('README.md', data)
+    })
+}
 
 // Function call to initialize app
 init();
